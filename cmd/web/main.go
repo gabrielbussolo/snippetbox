535ae5a30fa0ec0d5fc9f3f3512da4be1240c68c
Gabriel Bussolo <contact@gabrielbussolo.dev>
2024-06-06 10:01:25 -0300

    add static files to the template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatPost)
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
